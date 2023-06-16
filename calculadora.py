36665aef980f06bfaf153c73d60001f6d51099b7
Sansotelo <sansotelo77@gmail.com>
2023-06-16 09:31:40 -0300

    +calculadora

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+"""
+Atividade Calculadora
+"""
+
+numa = float(input("Primeiro valor: "))
+numb = float(input("Segundo valor: "))
+
+print(f"{numa} + {numb} = {numa+numb}")
+print(f"{numa} - {numb} = {numa-numb}")
+print(f"{numa} x {numb} = {numa*numb}")
+print(f"{numa} / {numb} = {numa/numb:.2f}")
+print(f"{numa} // {numb} = {numa//numb}")
+print(f"{numa} % {numb} = {numa%numb}")
+print(f"{numa} ^ {numb} = {numa**numb}")
